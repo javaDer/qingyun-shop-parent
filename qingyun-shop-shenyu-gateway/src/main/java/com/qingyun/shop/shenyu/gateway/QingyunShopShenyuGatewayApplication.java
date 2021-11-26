bd912a3ee9356e2e12a8de371fd7e4e9554b5433
zhaofa <fa20091001@163.com>
2021-11-26 21:20:58 +0800

    搭建ShenYu 网关

---
+++ qingyun-shop-shenyu-gateway/src/main/java/com/qingyun/shop/shenyu/gateway/QingyunShopShenyuGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.qingyun.shop.shenyu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingyunShopShenyuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingyunShopShenyuGatewayApplication.class, args);
+    }
+
+}
